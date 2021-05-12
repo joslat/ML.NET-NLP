f6422edc1fc68cb25d46e11f392e72307cf95bae
Jose Luis Latorre Millas <joslat@gmail.com>
2021-05-12 23:19:30 +0200

    some base stuff

---
+++ ML.NET-NLP/ML.NET-NLP-Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET_NLP_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
